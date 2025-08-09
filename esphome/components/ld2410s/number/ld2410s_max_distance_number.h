05613e4a57f6c43e07bf48d4f40dcbb79e0a26e4
NovakI <ivan.novak.rs@gmail.com>
2025-08-09 18:17:29 +0000

    align number

--- esphome/components/ld2410s/number/ld2410s_max_distance_number.h
+++ esphome/components/ld2410s/number/ld2410s_max_distance_number.h
@@ -6,16 +6,16 @@
 namespace esphome {
 namespace ld2410s {
 
-class LD2410SMaxDistanceNumber : public number::Number, public Parented<LD2410S> {
- public:
-  LD2410SMaxDistanceNumber() = default;
+// class LD2410SMaxDistanceNumber : public number::Number, public Parented<LD2410S> {
+//  public:
+//   LD2410SMaxDistanceNumber() = default;
 
- protected:
-  void control(float max_distance) override {
-    this->publish_state(max_distance);
-    this->parent_->set_max_distance(max_distance);
-  }
-};
+//  protected:
+//   void control(float max_distance) override {
+//     this->publish_state(max_distance);
+//     this->parent_->set_max_distance(max_distance);
+//   }
+// };
 
 }  // namespace ld2410s
 }  // namespace esphome

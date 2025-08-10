e5ff73f55a1905b20fb7a5d7b51102ef6d57552a
NovakI <ivan.novak.rs@gmail.com>
2025-08-10 06:23:15 +0000

    help

--- esphome/components/ld2410s/ld2410s_const.h
+++ esphome/components/ld2410s/ld2410s_const.h
@@ -1,10 +1,7 @@
-#include "ld2410s.h"
 
 #pragma once
 
 #include "esphome/core/application.h"
-// #include "esphome/core/defines.h"
-// #include <functional>
 
 namespace esphome {
 namespace ld2410s {

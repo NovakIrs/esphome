4cf47d0db733609253b48138f50d2f2c997b926d
NovakI <ivan.novak.rs@gmail.com>
2025-08-10 22:25:55 +0000

    rx

--- esphome/components/ld2410s/ld2410s_rx.cpp
+++ esphome/components/ld2410s/ld2410s_rx.cpp
@@ -75,8 +75,10 @@ EvaluationResult LD2410Srx::evaluate_() {
     case EvaluationResult::NOK:  // size matches expected size, but footer does not match expected footer for frame type
       this->hex_diag("<", &this->rcv_buffer_[0], this->end_pos_ + 1);
       ESP_LOGD(TAG,
-               "correct header and size, but footer does not match expected: size:%d, expected:%d, payload_size: %d",
-               this->end_pos_, this->expected_frame_size_, this->payload_size_);
+               "correct header and size, but footer does not match expected: size:%d, expected:%d, payload:%d, "
+               "head/foot:%d, size:%d",
+               this->end_pos_ + 1, this->expected_frame_size_, this->payload_size_, this->header_footer_size_,
+               this->size_field_size_);
       return EvaluationResult::NOK;
 
     case EvaluationResult::UNKNOWN:  // size less then expected size for frame type

65080cd3f804b210a968c1e594e6015840a12b26
NovakI <ivan.novak.rs@gmail.com>
2025-08-12 22:42:17 +0000

    tx

--- esphome/components/ld2410s/ld2410s.h
+++ esphome/components/ld2410s/ld2410s.h
@@ -47,36 +47,7 @@ namespace ld2410s {
 
 // using namespace ld24xx;
 
-enum class CmdState { EMPTY, SCHEDULED, SENT };
-
-struct TxFrameT {
-  uint8_t data[128];
-  uint32_t header;
-  uint32_t footer;
-  uint16_t length;
-  uint16_t command;
-  uint16_t data_length;
-};
-
-struct TxTaskT {
-  uint32_t time_started;
-  uint8_t retry;
-  CmdState state = CmdState::EMPTY;
-  TxFrameT *cmd_frame;
-};
-
-struct ThresholdsT {
-  uint32_t trigger[16];
-  uint32_t hold[16];
-  uint32_t snr[16];
-  uint8_t selected_gate{0};
-};
-// Constants
-
-static const uint16_t NO_SUB_CMD = 0xffff;
-static const uint8_t CMD_EXEC_BUFFER_SIZE = 32;
-
-class LD2410S : public Component, public uart::UARTDevice, LD2410Shelp {
+class LD2410S : public Component, public LD2410Shelp {
 #ifdef USE_SENSOR
   SUB_SENSOR(calibration_progress)
   SUB_SENSOR(distance)
@@ -141,44 +112,19 @@ class LD2410S : public Component, public uart::UARTDevice, LD2410Shelp {
 #endif
 
  protected:
-  LD2410Srx rx_;
+  SettingsT settings_;
   LD2410Stx tx_;
-  size_t rcv_end_pos_ = 0;
-  uint32_t max_dist_{0};
-  uint32_t min_dist_{0};
-  uint32_t delay_{0};
-  uint32_t status_freq_{0};
-  uint32_t dist_freq_{0};
-  uint32_t resp_speed_{0};
+  LD2410Srx rx_;
+
+  uint8_t init_status_{0};
+
   uint32_t energy_values_[16];
-
-  uint8_t active_ = 0;
-  uint8_t last_ = 0;
-  uint8_t init_status_ = 0;
-  bool minimal_output_{true};
-  bool tx_active_{false};
-
   std::string energy_values_str_ = "";
-  TxTaskT commands_[CMD_EXEC_BUFFER_SIZE];
-  ThresholdsT thresholds_;
 
   void init_();
 
   void send_();
-  void schedule_cmd_(const char *msg, uint16_t command, uint16_t sub_command = NO_SUB_CMD);
-  void schedule_cmd_frame_(uint16_t command, uint16_t sub_command = NO_SUB_CMD);
-  void cmd_frame_append_data_(TxFrameT *cmd_frame, const uint8_t *append_data, size_t append_data_size);
-  void cmd_frame_append_data_(TxFrameT *cmd_frame, const uint16_t *append_data, size_t append_data_size);
-  void cmd_frame_append_data_(TxFrameT *cmd_frame, const uint32_t *append_data, size_t append_data_size);
 
-  void cmd_buffer_insert_(TxFrameT *cmd_frame);
-  void cmd_buffer_finished_();
-  void cmd_buffer_inc_(uint8_t &index);
-
-  void loop_send_command_();
-  void send_command_(TxFrameT *cmd_frame);
-
-  bool receive_();
   void process_();
   void process_short_data_frame_();
   void process_data_frame_();
--- esphome/components/ld2410s/ld2410s_tx.cpp
+++ esphome/components/ld2410s/ld2410s_tx.cpp
@@ -2,5 +2,323 @@
 #include "ld2410s_tx.h"
 
 namespace esphome {
-namespace ld2410s {}  // namespace ld2410s
+namespace ld2410s {
+
+void LD2410Stx::schedule_cmd_(const char *msg, uint16_t command, uint16_t sub_command) {
+  ESP_LOGD(TAG, "schedule_cmd_: %s : %x : %x", msg, command, sub_command);
+
+  this->schedule_cmd_frame_(CONFIG_MODE_START_CMD);
+  this->schedule_cmd_frame_(command, sub_command);
+  this->schedule_cmd_frame_(CONFIG_MODE_END_CMD);
+}
+void LD2410Stx::schedule_cmd_frame_(uint16_t command, uint16_t sub_command) {
+  ESP_LOGD(TAG, "schedule_cmd_frame %x : %x", command, sub_command);
+
+  TxFrameT cmd_frame = {.header = CMD_FRAME_HEADER, .footer = CMD_FRAME_FOOTER, .command = command, .data_length = 0};
+
+  switch (command) {
+    case OUTPUT_MODE_SWITCH_CMD: {
+      if (this->settings_.minimal_output) {
+        this->cmd_frame_append_data_(&cmd_frame, &OUTPUT_MODE_VALUE_MIN[0], 4);
+      } else {
+        this->cmd_frame_append_data_(&cmd_frame, &OUTPUT_MODE_VALUE_STD[0], 4);
+      }
+    } break;
+
+    case CONFIG_MODE_START_CMD:
+      this->cmd_frame_append_data_(&cmd_frame, &CONFIG_MODE_START_VALUE[0], 2);
+      break;
+
+    case CONFIG_MODE_END_CMD:
+      break;
+
+    case PARAMS_READ_CMD:
+
+      switch (sub_command) {
+        case CFG_MAX_DETECTION_VALUE:
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_MAX_DETECTION_VALUE, 1);
+          break;
+
+        case CFG_MIN_DETECTION_VALUE:
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_MIN_DETECTION_VALUE, 1);
+          break;
+
+        case CFG_NO_DELAY_VALUE:
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_NO_DELAY_VALUE, 1);
+          break;
+
+        case CFG_STATUS_FREQ_VALUE:
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_STATUS_FREQ_VALUE, 1);
+          break;
+
+        case CFG_DISTANCE_FREQ_VALUE:
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_DISTANCE_FREQ_VALUE, 1);
+          break;
+
+        case CFG_RESPONSE_SPEED_VALUE:
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_RESPONSE_SPEED_VALUE, 1);
+          break;
+
+        default:
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_MAX_DETECTION_VALUE, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_MIN_DETECTION_VALUE, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_NO_DELAY_VALUE, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_STATUS_FREQ_VALUE, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_DISTANCE_FREQ_VALUE, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &CFG_RESPONSE_SPEED_VALUE, 1);
+          break;
+      }
+
+      break;
+
+    case FW_READ_CMD:
+      break;
+
+    case PARAMS_WRITE_CMD:
+      if (this->settings_.resp_speed == 0) {
+        ESP_LOGD(TAG, "PARAMS_WRITE_CMD Error, bad new_config");
+        return;
+      } else {
+        switch (sub_command) {
+          case CFG_MAX_DETECTION_VALUE:
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_MAX_DETECTION_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.max_dist, 1);
+            break;
+
+          case CFG_MIN_DETECTION_VALUE:
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_MIN_DETECTION_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.min_dist, 1);
+            break;
+
+          case CFG_NO_DELAY_VALUE:
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_NO_DELAY_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.delay, 1);
+            break;
+
+          case CFG_STATUS_FREQ_VALUE:
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_STATUS_FREQ_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.status_freq, 1);
+            break;
+
+          case CFG_DISTANCE_FREQ_VALUE:
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_DISTANCE_FREQ_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.dist_freq, 1);
+            break;
+
+          case CFG_RESPONSE_SPEED_VALUE:
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_RESPONSE_SPEED_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.resp_speed, 1);
+            break;
+
+          default:
+
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_MAX_DETECTION_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.max_dist, 1);
+
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_MIN_DETECTION_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.min_dist, 1);
+
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_NO_DELAY_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.delay, 1);
+
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_STATUS_FREQ_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.status_freq, 1);
+
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_DISTANCE_FREQ_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.dist_freq, 1);
+
+            this->cmd_frame_append_data_(&cmd_frame, &CFG_RESPONSE_SPEED_VALUE, 1);
+            this->cmd_frame_append_data_(&cmd_frame, &this->settings_.resp_speed, 1);
+
+            break;
+        }
+        break;
+      }
+
+    case CALIBRATION_CMD:
+      this->cmd_frame_append_data_(&cmd_frame, &CALIBRATION_TRIGGER_VALUE, 1);
+      this->cmd_frame_append_data_(&cmd_frame, &CALIBRATION_RETENTION_VALUE, 1);
+      this->cmd_frame_append_data_(&cmd_frame, &CALIBRATION_TIME_VALUE, 1);
+      break;
+
+    case GATE_THRESHOLD_TRIGGER_READ_CMD:
+    case GATE_THRESHOLD_HOLD_READ_CMD:
+    case GATE_THRESHOLD_SNR_READ_CMD:
+      if (sub_command != NO_SUB_CMD) {
+        this->cmd_frame_append_data_(&cmd_frame, &sub_command, 1);
+      } else {
+        for (uint16_t i = 0; i < 16; i++) {
+          this->cmd_frame_append_data_(&cmd_frame, &i, 1);
+        }
+      }
+      break;
+
+    case GATE_THRESHOLD_TRIGGER_WRITE_CMD:
+      if (sub_command != NO_SUB_CMD) {
+        this->cmd_frame_append_data_(&cmd_frame, &sub_command, 1);
+        this->cmd_frame_append_data_(&cmd_frame, &this->settings_.thresholds.trigger[sub_command], 1);
+      } else {
+        for (uint16_t i = 0; i < 16; i++) {
+          this->cmd_frame_append_data_(&cmd_frame, &i, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &this->settings_.thresholds.trigger[i], 1);
+        }
+      }
+      break;
+
+    case GATE_THRESHOLD_HOLD_WRITE_CMD:
+      if (sub_command != NO_SUB_CMD) {
+        this->cmd_frame_append_data_(&cmd_frame, &sub_command, 1);
+        this->cmd_frame_append_data_(&cmd_frame, &this->settings_.thresholds.hold[sub_command], 1);
+      } else {
+        for (uint16_t i = 0; i < 16; i++) {
+          this->cmd_frame_append_data_(&cmd_frame, &i, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &this->settings_.thresholds.hold[i], 1);
+        }
+      }
+      break;
+
+    case GATE_THRESHOLD_SNR_WRITE_CMD:
+      if (sub_command != NO_SUB_CMD) {
+        this->cmd_frame_append_data_(&cmd_frame, &sub_command, 1);
+        this->cmd_frame_append_data_(&cmd_frame, &this->settings_.thresholds.snr[sub_command], 1);
+      } else {
+        for (uint16_t i = 0; i < 16; i++) {
+          this->cmd_frame_append_data_(&cmd_frame, &i, 1);
+          this->cmd_frame_append_data_(&cmd_frame, &this->settings_.thresholds.snr[i], 1);
+        }
+      }
+      break;
+
+    default:
+
+      break;
+  }
+
+  this->cmd_buffer_insert_(&cmd_frame);
+}
+void LD2410Stx::cmd_frame_append_data_(TxFrameT *cmd_frame, const uint8_t *append_data, size_t append_data_size) {
+  memcpy(&cmd_frame->data[0] + cmd_frame->data_length * sizeof(cmd_frame->data[0]), append_data,
+         append_data_size * sizeof(*append_data));
+
+  cmd_frame->data_length = cmd_frame->data_length + append_data_size * sizeof(*append_data);
+}
+void LD2410Stx::cmd_frame_append_data_(TxFrameT *cmd_frame, const uint16_t *append_data, size_t append_data_size) {
+  memcpy(&cmd_frame->data[0] + cmd_frame->data_length * sizeof(cmd_frame->data[0]), append_data,
+         append_data_size * sizeof(*append_data));
+
+  cmd_frame->data_length = cmd_frame->data_length + append_data_size * sizeof(*append_data);
+}
+void LD2410Stx::cmd_frame_append_data_(TxFrameT *cmd_frame, const uint32_t *append_data, size_t append_data_size) {
+  memcpy(&cmd_frame->data[0] + cmd_frame->data_length * sizeof(cmd_frame->data[0]), append_data,
+         append_data_size * sizeof(*append_data));
+
+  cmd_frame->data_length = cmd_frame->data_length + append_data_size * sizeof(*append_data);
+}
+
+void LD2410Stx::cmd_buffer_insert_(TxFrameT *cmd_frame) {
+  if (!cmd_frame) {
+    return;
+  }
+
+  TxTaskT cmd;
+  cmd.state = CmdState::SCHEDULED;
+  cmd.cmd_frame = cmd_frame;
+  cmd.time_started = 0;
+  cmd.retry = 0;
+
+  if (this->commands_[this->last_].state != CmdState::EMPTY) {
+    uint8_t next = this->last_;
+    this->cmd_buffer_inc_(next);
+    if (this->commands_[next].state != CmdState::EMPTY) {
+      return;
+    }
+    this->last_ = next;
+  }
+
+  this->commands_[this->last_] = cmd;  // Shallow copy of state, time_started, retry
+
+  if (cmd.cmd_frame) {
+    this->commands_[this->last_].cmd_frame = new TxFrameT(*cmd.cmd_frame);  // Deep copy
+  } else {
+    this->commands_[this->last_].cmd_frame = nullptr;
+  }
+}
+void LD2410Stx::cmd_buffer_finished_() {
+  this->commands_[this->active_].state = CmdState::EMPTY;
+
+  if (this->commands_[this->active_ + 1].state != CmdState::EMPTY) {
+    this->cmd_buffer_inc_(this->active_);
+  }
+}
+void LD2410Stx::cmd_buffer_inc_(uint8_t &index) {
+  index++;
+  if (index >= CMD_EXEC_BUFFER_SIZE) {
+    index = 0;
+  }
+}
+
+void LD2410Stx::loop_send_command_() {
+  TxTaskT *cmd = &commands_[this->active_];
+  uint32_t now = App.get_loop_component_start_time();
+
+  if (cmd->state == CmdState::SCHEDULED) {
+    this->send_command_(cmd->cmd_frame);
+    cmd->state = CmdState::SENT;
+    cmd->time_started = now;
+
+  } else if (cmd->state == CmdState::SENT) {
+    if (now >= cmd->time_started + CMD_EXEC_TIMEOUT) {
+      if (cmd->retry < CMD_EXEC_REPEAT) {
+        ESP_LOGD(TAG, "SendCmd Retry active:%d, last:%d", this->active_, this->last_);
+        cmd->retry++;
+        cmd->time_started = now;
+        this->send_command_(cmd->cmd_frame);
+      } else {
+        ESP_LOGD(TAG, "SendCmd GivingUp active:%d, last:%d", this->active_, this->last_);
+        cmd->state = CmdState::EMPTY;
+        this->cmd_buffer_finished_();
+      }
+    }
+  } else if (cmd->state == CmdState::EMPTY && this->active_ == this->last_ && this->active_ != 0) {
+    this->active_ = 0;
+    this->last_ = 0;
+  }
+}
+void LD2410Stx::send_command_(TxFrameT *frame) {
+  char output[64];
+  sprintf(output, "SendingCommand: %02X", frame->command);
+
+  uint8_t tx_buffer[128];
+
+  frame->length = 0;
+  uint16_t frame_data_bytes = frame->data_length + 2;
+  // HEADER
+  memcpy(&tx_buffer[frame->length], &frame->header, sizeof(frame->header));
+  frame->length += sizeof(frame->header);
+  // SIZE
+  memcpy(&tx_buffer[frame->length], &frame_data_bytes, sizeof(frame->data_length));
+  frame->length += sizeof(frame->data_length);
+  // COMMAND
+  memcpy(&tx_buffer[frame->length], &frame->command, sizeof(frame->command));
+  frame->length += sizeof(frame->command);
+  // DATA
+  for (uint16_t index = 0; index < frame->data_length; index++) {
+    memcpy(&tx_buffer[frame->length], &frame->data[index], sizeof(frame->data[index]));
+    frame->length += sizeof(frame->data[index]);
+  }
+  // FOOTER
+  memcpy(tx_buffer + frame->length, &frame->footer, sizeof(frame->footer));
+  frame->length += sizeof(frame->footer);
+
+  this->hex_diag(">", tx_buffer, frame->length);
+
+  // WRITE
+  for (uint16_t index = 0; index < frame->length; index++) {
+    this->write_byte(tx_buffer[index]);
+  }
+
+  this->flush();
+}
+
+}  // namespace ld2410s
 }  // namespace esphome

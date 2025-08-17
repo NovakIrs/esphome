d4214af7384c0b288708526e9cd4a6bdbe51815f
NovakI <ivan.novak.rs@gmail.com>
2025-08-17 11:57:32 +0000

    log

--- esphome/components/ld2410s/ld2410s_tx.cpp
+++ esphome/components/ld2410s/ld2410s_tx.cpp
@@ -18,6 +18,7 @@ void LD2410Stx::cmd_buffer_insert(TxFrameT *cmd_frame) {
     this->error_ = false;
   }
 
+  ESP_LOGE(TAG, "Inserting command %x into command buffer at position %d", cmd_frame->command, this->last_);
   this->commands_[this->last_].state = CmdState::SCHEDULED;
   this->commands_[this->last_].time_started = 0;
   this->commands_[this->last_].retry = 0;

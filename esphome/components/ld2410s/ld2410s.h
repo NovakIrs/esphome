31b7d727fc831505af0815f3e36c735398952e0d
NovakI <ivan.novak.rs@gmail.com>
2025-08-17 15:07:17 +0000

    fix

--- esphome/components/ld2410s/ld2410s.h
+++ esphome/components/ld2410s/ld2410s.h
@@ -148,7 +148,7 @@ class LD2410S : public Component, public uart::UARTDevice, LD2410Shelp {
   void schedule_cmd_sequence_(const char *msg, uint16_t command, uint16_t sub_command = NO_SUB_CMD);
   void schedule_cmd_frame_(uint16_t command, uint16_t sub_command = NO_SUB_CMD);
   template<typename T>
-  bool cmd_frame_append_data_(uint8_t *data, uint16_t &data_length, const T *append_data, size_t append_data_size);
+  bool cmd_frame_append_data_(uint8_t *data, uint16_t &data_length, const T *append_data, size_t append_data_size = 1);
 
   void process_();
   void process_short_data_frame_();

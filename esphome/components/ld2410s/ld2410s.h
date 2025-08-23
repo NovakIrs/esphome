15444d6dc616f5838d21a2c5e1f6b70bf9b5ce7f
NovakI <ivan.novak.rs@gmail.com>
2025-08-23 17:44:41 +0000

    dc

--- esphome/components/ld2410s/ld2410s.h
+++ esphome/components/ld2410s/ld2410s.h
@@ -3,8 +3,8 @@
 #define LD2410S_V1
 #define LD2410S_V2
 // #define LD2410S_DEBUG_UART
-#define LD2410S_ENABLE_UART
-// #define LD2410S_ENABLE_DC
+// #define LD2410S_ENABLE_UART
+#define LD2410S_ENABLE_DC
 
 // core
 #include "esphome/core/application.h"

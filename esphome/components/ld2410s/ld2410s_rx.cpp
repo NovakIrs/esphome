4def78939b1e803aab1019578d1846c5ac5fbab0
NovakI <ivan.novak.rs@gmail.com>
2025-08-24 07:46:13 +0000

    .c_str()

--- esphome/components/ld2410s/ld2410s_rx.cpp
+++ esphome/components/ld2410s/ld2410s_rx.cpp
@@ -40,7 +40,7 @@ RxEvaluationResult LD2410Srx::receive_byte(uint32_t loop_count, uint8_t byte) {
     case RxEvaluationResult::NOK:
     default:
 #ifdef LD2410S_DEBUG_UART
-      ESP_LOGE(TAG, "<XX [%d] %s < %s", loop_count, this->msg_,
+      ESP_LOGE(TAG, "<XX [%d] %s < %s", loop_count, this->msg_.c_str(),
                format_hex_pretty(this->rcv_buffer_, end_pos_ + 1, ' ').c_str());
 #endif
       this->reset_();

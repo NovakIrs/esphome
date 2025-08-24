24e11e1df921647fa47dd91ca148f7c52ee54ca6
NovakI <ivan.novak.rs@gmail.com>
2025-08-24 13:41:17 +0000

    log

--- esphome/components/ld2410s/ld2410s_rx.cpp
+++ esphome/components/ld2410s/ld2410s_rx.cpp
@@ -39,8 +39,10 @@ RxEvaluationResult LD2410Srx::receive_byte(uint32_t loop_count, uint8_t byte) {
     case RxEvaluationResult::NOK:
     default:
 #ifdef LD2410S_DEBUG_UART
+#ifdef LD2410S_SHOW_DC
       ESP_LOGE(TAG, "<XX [%d] %s < %s", loop_count, this->msg_.c_str(),
                format_hex_pretty(this->rcv_buffer_, end_pos_ + 1, ' ').c_str());
+#endif
 #endif
       this->reset_();
       result = RxEvaluationResult::UNKNOWN;

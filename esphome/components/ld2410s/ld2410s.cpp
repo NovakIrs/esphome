efc32b710d52e60a304be3fbbabaf9875ba7c4a3
NovakI <ivan.novak.rs@gmail.com>
2025-08-25 22:36:22 +0000

    strategy 1 upgrade

--- esphome/components/ld2410s/ld2410s.cpp
+++ esphome/components/ld2410s/ld2410s.cpp
@@ -57,6 +57,9 @@ void LD2410S::send_() {
           ESP_LOGE(TAG, "RECOVER STRATEGY 1 - REBOOT LD2410S ?");
           static const uint8_t reboot_cmd[] = {0xF8, 0xF8, 0x04, 0x00, 0x0B, 0x00, 0x0B, 0x00};
           this->write_array(reboot_cmd, sizeof(reboot_cmd));
+          this->tx_schedule_.reset();
+          this->tx_schedule_.append(CONFIG_MODE_END_CMD);
+          this->tx_schedule_.append(CONFIG_MODE_START_CMD);
           break;
 
         case 2:

d876c0c396cf468295927081b9d2cb15d2bb048a
NovakI <ivan.novak.rs@gmail.com>
2025-08-25 23:32:13 +0000

    fix

--- esphome/components/ld2410s/ld2410s.cpp
+++ esphome/components/ld2410s/ld2410s.cpp
@@ -72,7 +72,7 @@ void LD2410S::send_() {
           break;
 
         case 3:
-          ESP_LOGE(TAG, "RECOVER STRATEGY 3 - REBOOT LD2410S V2?");
+          ESP_LOGE(TAG, "RECOVER STRATEGY 3 - REBOOT LD2410S V3?");
           static const uint8_t reboot_cmd_v2[] = {0xFD, 0xFC, 0xFB, 0xFA, 0x02, 0x00,
                                                   0xA2, 0x00, 0x04, 0x03, 0x02, 0x01};
           this->write_array(reboot_cmd_v2, sizeof(reboot_cmd_v2));

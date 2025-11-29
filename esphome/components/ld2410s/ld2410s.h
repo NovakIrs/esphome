1e38684a9aef9a86ed5fe2203ce108c3fb3e7d1f
NovakI <ivan.novak.rs@gmail.com>
2025-11-29 15:35:45 +0000

    fix includes

--- esphome/components/ld2410s/ld2410s.h
+++ esphome/components/ld2410s/ld2410s.h
@@ -4,7 +4,6 @@
 
 // core
 #include "esphome/core/application.h"
-// #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
 #include "esphome/core/defines.h"
 #include "esphome/core/helpers.h"
@@ -40,11 +39,8 @@
 #endif
 
 // std
-// #include <functional>
-// #include <iomanip>
 #include <cstddef>
 #include <cstdint>
-// #include <type_traits>
 
 namespace esphome::ld2410s {
 
